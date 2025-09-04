c5924e572583b255f9bf7014cb22f4258f1e6246
hdvibha <hdvibha@gmail.com>
2025-09-04 20:50:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  // Show surprise message on home
+  $("#showMessage").click(function(){
+    $("#message").fadeToggle();
+  });
+
+  // Accordion for skills
+  $(".acc-btn").click(function(){
+    $(this).next(".acc-content").slideToggle();
+  });
+});
